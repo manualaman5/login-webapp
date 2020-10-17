1314c957f40fa8a79bb7cbc5ddff651dc918a2f8
manualaman5 <manualaman5@gmail.com>
2020-10-17 18:21:09 +0000

    Updated logic in main.py and added three html templates

--- loginAppEngine/main.py
+++ loginAppEngine/main.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask import request, redirect, render_template
 import flask
 app = Flask(__name__)
+
+#The default route renders the index and shows the form
 @app.route('/')
 def displayIndexPage():
     return flask.send_file("index.html")
+
+"""
+This is the route for the database viewer, when the user clicks the database button in the index, this route will query the database and show the records in the logins table.
+"""
 @app.route('/databaseViewer')
 def displayDatabase():
     query = "SELECT * FROM logins;"
@@ -28,51 +34,60 @@ def displayDatabase():
     finally:
         connection.close()
         return message
+
+"""
+This is the route for the login, when the user sends the form, this route will check if the user is correct, if they are a trainer and insert the record in logins accordingly.
+"""
 @app.route('/newEntry', methods=["POST", "GET"])
 def addNewUser():
     if (request.method == "GET"):
         print (request.remote_addr)
         return "Please get back to the front page and specify your ldap"
     elif (request.method == "POST"):
-        trainers = ["tester", "another"]
+        #This will be the list of the trainers
+        trainers = ["malaman"]
+
+        #Get the variables from the request
         ldap = request.form["ldap"]
         pw = request.form["pw"]
         ip = request.environ['REMOTE_ADDR']
         # ADDTIME converts from UTC to UTC+2
         # change this command when the clock changes in Spain!
+        
         insert_query = f"INSERT INTO logins (ldap, ip, timestamp) VALUES ('{ldap}', '{ip}', ADDTIME (CURRENT_TIMESTAMP(), '2:0:0'));"
-        password_query = f"SELECT * from passwords where ldap="
+        
         import pymysql
         connection = pymysql.connect(
             unix_socket='/cloudsql/wave34-webhelp-vrodriguez:europe-west1:mysql', user='andrew', password='sirAndrew', db="improvedArrivalRegister")
-        # THIS IS TO CHECK THE USER for trainees
-        if pw:
-            print("I have a password")
-            message = ""
-            try:
-                password_query +=  "'" + ldap + "' AND password =" + "'" + pw + "';"
-                print(password_query)
-                with connection.cursor() as cur:
-                    print(password_query)
-                    cur.execute(password_query)
-                    data = cur.fetchall()
-                    print(data, type(data))
-                if data:
-                    # processing and insert record
+        try:
+            password_query = f"SELECT * FROM passwords WHERE ldap='{ldap}' 'ldap' AND password ='{pw}';"
+            #print('This is the password query',password_query)
+            
+            with connection.cursor() as cur:
+                cur.execute(password_query)
+                data = cur.fetchall()
+            
+            #When the combination of user and password is not in the database, we will render the wrong credentials template.
+            if not data:
+                return render_template("wrongcred.html")
+            #When the combination is in the database, we will check if they are a trainer or insert the login record
+            else:
+                if ldap not in trainers:
+                    print("This is not a trainer")
                     with connection.cursor() as cur:
                         cur.execute(insert_query)
                         connection.commit()
-                        message += "RECORD INSERTED"
+                        print ("RECORD INSERTED")
+                    return render_template("success.html")
                 else:
-                    # add error here
-                    message += "The credentials provided are not correct"
-            except Exception as e:
-                print (e)
-            finally:
-                connection.close()
-                return message
-        # This part will be for the trainers
-        elif ldap in trainers:
-            return "this is a trainer"
+                    print("This is a trainer")
+                    return render_template("trainers.html")
+        except Exception as e:
+            print (e)
+        finally:
+            connection.close()
+            print('Connection closed!')
+
+
 if (__name__ == '__main__'):
     app.run(host='0.0.0.0', port=5000)
